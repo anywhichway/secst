e86aa1ae83322e066d91b085a866b9b6422644d0
syblackwell <syblackwell@anywhichway.com>
2022-12-16 08:51:01 -0800

    Added section.js to git

---
+++ src/tags/section.js
@@ -0,0 +1,48 @@
+import Tag from "../tag.js";
+import blockContent from "./block-content.js";
+
+const section = {
+    stringAllowed: true,
+    contentAllowed: {
+        ...blockContent,
+    },
+    transform(node,{level}) {
+        const pconfig = section.contentAllowed.p;
+        let p;
+         node.content = [...node.content.reduce((content,item,i) => {
+             if(typeof(item)==="string") {
+                 const paragraphs = item.split("\r\n\r\n");
+                 if(paragraphs.length>1) {
+                     paragraphs.forEach((paragraph,i) => {
+                         if(i===0 && p) {
+                             p.content.push(paragraph);
+                         } else {
+                             content.add(p = new Tag({tag: "p", content: [paragraph]}))
+                         }
+                     })
+                 } else {
+                     p ||= new Tag({tag:"p",level});
+                     p.content.push(item);
+                 }
+             } else if(pconfig.contentAllowed[item.tag]) {
+                 p ||= new Tag({tag:"p",level});
+                 p.content.push(item);
+             } else if(section.contentAllowed[item.tag]) {
+                 content.add(item);
+                 p = new Tag({tag:"p",level});
+            } else {
+                 console.log("err")
+             }
+            if(p) {
+                content.add(p);
+            }
+            return content;
+        },new Set())].filter((item) => {
+            return item.tag!=="p" || item.content.length>1 || (item.content.length>0 && (typeof(item.content[0])!=="string" || item.content[0].trim().length>0));
+         });
+        return node;
+    }
+}
+
+
+export {section,section as default}
